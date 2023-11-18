e72cd76d0317a96995417fb31e10780d2dcab8a6
Hamza <jhamzaahmed@gmail.com>
2023-11-18 19:23:22 +0500

    learned XMLHttpRequest

--- asynchronous programming/asynchronous_prog_02.js
+++ asynchronous programming/asynchronous_prog_02.js
@@ -5,3 +5,106 @@
 
 const xhr = new XMLHttpRequest();
 console.log(xhr);
+
+// a fake API for testing
+const URL = 'https://jsonplaceholder.typicode.com/posts';
+
+// readyState can have five values i.e : from 0 to 4
+// before using the open method, readyState is 0
+console.log(xhr.readyState)     // 0
+
+xhr.open('GET', URL);
+
+// after using the open method, readyState is 1
+console.log(xhr.readyState)     // 1
+
+// after 1, rest of the states are displayed
+// 2 for send()
+// 3 for LOADING
+// 4 for DONE
+// info about readyState can be viewed on mdn docs
+xhr.onreadystatechange = function(){    // 2, 3, 4
+    console.log(xhr.readyState);
+    if(xhr.readyState === 4){
+        // console.log(xhr.response);
+
+        const response = xhr.response;     // response gives the entire JSON to us
+        console.log(typeof response);       // the returned JSON is always a string
+        const data = JSON.parse(response);  // converting string back to object
+        console.log(typeof data)
+    }
+}
+
+// an alternative to the above lines of code is using the onload.
+// the onload checks the readystate by itselt and is only executed
+// if readyState == 4, so we don't have to explicitly check the state by ourselves.
+xhr.onload = function(){
+    console.log(xhr.readyState);
+}
+
+xhr.send();
+
+
+
+// error handling in XHR 
+
+// wrong URL
+const newURL = 'https://jsonplaceholder.typicode.com/postss';
+
+const newXHR = new XMLHttpRequest();
+
+newXHR.open('GET', newURL);
+
+newXHR.onload = ()=> {
+    if(newXHR.status >= 200 && newXHR.status < 300){    // checking for error
+        const data = JSON.parse(newXHR.response);
+        console.log(data);
+    }
+    else {
+        console.log('something went wrong');
+    }
+}
+
+// for network related errors like no internet connection we can use onerror.
+newXHR.onerror = () => {
+    console.log('network error')
+}
+
+newXHR.send();
+
+
+
+// requesting a specific object in the entire JSON
+const newURL2 = 'https://jsonplaceholder.typicode.com/posts';
+const newXHR2 = new XMLHttpRequest();
+
+newXHR2.open('GET', newURL2);
+
+newXHR2.onload = ()=> {
+    if(newXHR2.status >= 200 && newXHR2.status < 300){    // checking for error
+        const data2 = JSON.parse(newXHR2.response);
+        
+        console.log(data2[3]);       // indexing 4th element from object
+        const id = data2[3].id;      // getting id from object
+        const xhr3 = new XMLHttpRequest();
+        const newURL3 = `${newURL2}/${id}`;     // creating new url
+        console.log(newURL3);       // checking new url
+
+        xhr3.open('GET', newURL3);      //  requesting new url by creating a new XMLHttpRequest
+        xhr3.onload = () => {
+            const data3 = JSON.parse(xhr3.response);
+            console.log(data3);     // now we get specific data that we requested
+        }
+        xhr3.send();
+    }
+    else {
+        console.log('something went wrong');
+    }
+}
+
+// for network related errors like no internet connection we can use onerror.
+newXHR2.onerror = () => {
+    console.log('network error')
+}
+
+newXHR2.send();
